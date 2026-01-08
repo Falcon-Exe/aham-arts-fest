1ca00df8e3771c0a5bbefa252f9e68f354ec162c
Falcon-Exe <muhsabir.c@gmail.com>
2026-01-08 22:22:07 +0530

    Fix PWA manifest and install

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      injectRegister: "auto",
-
-      workbox: {
-        cleanupOutdatedCaches: true,
-        skipWaiting: true,
-        clientsClaim: true,
-      },
+  injectRegister: "auto",
+  devOptions: {
+    enabled: true
+  },
+  workbox: {
+    clientsClaim: true,
+    skipWaiting: true
+  },
 
       manifest: {
         name: "AHAM Arts Fest",
