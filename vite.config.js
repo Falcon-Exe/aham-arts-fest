d983594eb27005966b8326030b70cf06c7b8a18e
Falcon-Exe <muhsabir.c@gmail.com>
2026-01-08 22:39:47 +0530

    Fix Android PWA install

--- vite.config.js
+++ vite.config.js
@@ -2,44 +2,33 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
-  injectRegister: "auto",
-  devOptions: {
-    enabled: true
-  },
-  workbox: {
-    clientsClaim: true,
-    skipWaiting: true
-  },
+      injectRegister: "auto",
 
       manifest: {
         name: "AHAM Arts Fest",
         short_name: "AHAM",
         start_url: "/",
-        scope: "/",
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "pwa-192x192.png",
             sizes: "192x192",
-            type: "image/png",
-            purpose: "any maskable"
+            type: "image/png"
           },
           {
-            src: "/pwa-512x512.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
+            type: "image/png"
           }
         ]
       }
-    }),
-  ],
+    })
+  ]
 });
