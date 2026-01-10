c1c551d23bd0e1983b93555e5ec9d6a0e09ea83d
Falcon-Exe <muhsabir.c@gmail.com>
2026-01-10 21:11:28 +0530

    header 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,20 @@
+// import logo from "../assets/logo.jpg";
+
+// function Header() {
+//   return (
+//     <div style={{ textAlign: "center", padding: "30px 20px" }}>
+//       <img src={logo} alt="Fest Logo" width="500" />
+//       <h1 style={{ marginTop: "10px", fontSize: "32px" }}>
+//         AHAM ARTS FEST 2025–26
+//       </h1>
+//       <p style={{ color: "#555", marginTop: "5px" }}>
+//         Where talent meets tradition 🎭
+//       </p>
+//     </div>
+//   );
+// }
+
+// export default Header;
 import logo from "../assets/logo.jpg";
 import "./Header.css";
 
