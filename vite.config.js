8057832901540f48f8c7d94dec7ec1c461b5879e
Falcon-Exe <muhsabir.c@gmail.com>
2026-01-10 22:14:44 +0530

    Disable PWA to fix loading issue

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,47 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "auto",
-      workbox: {
-        cleanupOutdatedCaches: true,
-        skipWaiting: true,
-        clientsClaim: true,
-      },
-      manifest: {
-        name: "AHAM Arts Fest",
-        short_name: "AHAM",
-        start_url: "/",
-        scope: "/",
-        display: "standalone",
-theme_color: "#913831",
-background_color: "#ffffff",
-        icons: [
-          {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          },
+//     VitePWA({
+//       registerType: "autoUpdate",
+//       injectRegister: "auto",
+//       workbox: {
+//         cleanupOutdatedCaches: true,
+//         skipWaiting: true,
+//         clientsClaim: true,
+//       },
+//       manifest: {
+//         name: "AHAM Arts Fest",
+//         short_name: "AHAM",
+//         start_url: "/",
+//         scope: "/",
+//         display: "standalone",
+// theme_color: "#913831",
+// background_color: "#ffffff",
+//         icons: [
+//           {
+//             src: "/pwa-192x192.png",
+//             sizes: "192x192",
+//             type: "image/png"
+//           },
+//           {
+//             src: "/pwa-512x512.png",
+//             sizes: "512x512",
+//             type: "image/png"
+//           },
           
-        //   {
-        //     src: "/pwa-maskable-512.png",
-        //     sizes: "512x512",
-        //     type: "image/png",
-        //     purpose: "maskable"
-        //   }
-        ]
-      }
-    })
+//         //   {
+//         //     src: "/pwa-maskable-512.png",
+//         //     sizes: "512x512",
+//         //     type: "image/png",
+//         //     purpose: "maskable"
+//         //   }
+//         ]
+//       }
+//     })
   ]
 });
 
