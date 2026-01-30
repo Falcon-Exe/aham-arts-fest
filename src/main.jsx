e89ef593a31690ac421501816991eff03c1aa617
Falcon-Exe <muhsabir.c@gmail.com>
2026-01-30 14:35:48 +0530

    INSIGHT

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelmetProvider>
       <App />
+      <SpeedInsights />
     </HelmetProvider>
   </StrictMode>
 );
